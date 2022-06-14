24d4a8418095f5eadcb592b5b45b7485d6444533
DanielVZA <danielvasquez@protonmail.com>
2022-06-13 22:20:29 -0400

    Deploy Microservices Section Completed

--- react-ambassador/next-checkout/constants.ts
+++ react-ambassador/next-checkout/constants.ts
@@ -1,4 +1,4 @@
 export default {
-    endpoint: 'http://localhost:8002/api/checkout',
+    endpoint: 'http://35.244.203.7/api/checkout',
     stripe_key: 'pk_test_51L1cwjHuFtteLT4PtOz6VCuGGrHYX7f9Y5VnbQxCUvMJHUIgVAVqqrBAWiy0552iFJQzAXrBDMfFo6jeRxEu6CDy00xflNNi69'
 }
